6d6b31f758d2bf41c2d2c14d1c3736f2f97821cf
Pradeep HR <81664507+iampradeep-hr@users.noreply.github.com>
2022-11-17 22:36:44 +0530

    Node MCU ESP8266 & PIR Sensor code

---
+++ pir_code.ino
@@ -0,0 +1,69 @@
+
+#include<Adafruit_MQTT_Client.h>
+#include<ESP8266WiFi.h>
+
+#define wifi_name "Tor-999"
+#define pwd "Tom@#$jerry"
+#define server "io.adafruit.com"
+#define port 1883
+#define username "pradeep03"
+#define aiokey "aio_jYlZ59gDK4MPjNZhQBCh9iUesDdo"
+
+
+#define ledPin D0
+#define pirPin D1
+
+WiFiClient IoT;
+
+Adafruit_MQTT_Client mqtt(&IoT, server,port,username,aiokey);
+Adafruit_MQTT_Publish feed= Adafruit_MQTT_Publish(&mqtt, username"/feeds/pirdata");
+
+
+
+int pirValue;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  delay(10);
+  WiFi.begin(wifi_name,pwd);
+  while(WiFi.status () != WL_CONNECTED)
+  {
+   
+    Serial.print(".");
+  }
+  Serial.println(WiFi.localIP());
+  while(mqtt.connect())
+  {
+    Serial.print(".");
+  }
+    pinMode(ledPin,OUTPUT);
+    pinMode(pirPin,INPUT);
+    digitalWrite(ledPin,LOW);
+  
+}
+
+
+void loop(){
+  pirValue=digitalRead(pirPin);
+  digitalWrite(ledPin,pirValue);
+  Serial.println(pirValue);
+  int data=digitalRead(pirPin);
+  if(mqtt.connected()){
+
+  Serial.println("Sending ");
+  Serial.println(data);
+  if(feed.publish(data)){
+    Serial.println("Success");
+  }
+  else{
+    Serial.println("Trying");
+    }
+  }
+  if(data==1){
+    delay(4000);
+  }
+
+
+delay(2000);
+}
